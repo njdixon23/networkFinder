f38cce3b49e6d337876d934d2501cb726b9285d2
njdixon23 <nathan_dixon22@milton.edu>
2021-11-02 08:53:13 -0400

    version_0.1

---
+++ ipdata.js
@@ -0,0 +1,10 @@
+function json(url) {
+    return fetch(url).then(res => res.json());
+  }
+  
+  let apiKey = 'fb729aa07afc67f75787ec919286da17448183a2fbc17c8aed692f03';
+  json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+    console.log(data.ip);
+    console.log(data.city);
+    console.log(data.country_code);
+  });
