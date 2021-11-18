874d5da3a0d0fbf2b921446889ebcc3210f2719d
njdixon23 <nathan_dixon22@milton.edu>
2021-11-18 00:30:05 -0500

    version_0.4

--- popup.js
+++ popup.js
@@ -1,22 +1,31 @@
 document.getElementById("speedy").addEventListener("mouseover", changeDef);
 document.getElementById("speedy").addEventListener("mouseleave", removeText);
 document.getElementById("speed").addEventListener("mousedown", speedTest);
-function changeDef() {
 
+let ipInfo;
+let cityInfo;
+let countryInfo;
+
+function changeDef() {
   let ele = document.getElementById('speed_help');
   let node = document.createTextNode('For Finding Speed');
   ele.appendChild(node);
-
-
 }
+
+
+
 function removeText() {
   document.getElementById('speed_help').textContent = null;
 }
 
 function speedTest(){
-   console.log("Button Pressed...testing speed")
-
+  console.log("Button Pressed...testing speed")
   printInfo();
+  let speedEle = document.getElementById('ipP');
+  console.log("Element found")
+  let speedNode = document.createTextNode(ipInfo);
+  speedEle.appendChild(speedNode);
+  console.log("Info printed");
 }
 
 function json(url) {
@@ -27,8 +36,11 @@ function printInfo() {
   let apiKey = 'fb729aa07afc67f75787ec919286da17448183a2fbc17c8aed692f03';
   json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   console.log(data.ip);
+  ipInfo = data.ip;
   console.log(data.city);
+  cityInfo = data.city;
   console.log(data.country_code);
+  countryInfo = data.country_code;
 });
 }
 
